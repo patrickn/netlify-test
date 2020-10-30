2cd94b7ba6b80b7ae4e3fc9052556689edd46c00
Patrick <patrick@neavey.net>
2020-10-30 16:40:38 +0100

    updates

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 
-echo "<html><head></head><body>" > output.html
-echo "<h2>A List</h2><code>" >> output.html
-ls >> output.html
-echo "<code></body></html>" >> output.html
+echo "<html><head></head><body>" > public/output.html
+echo "<h2>A List</h2><code>" >> public/output.html
+ls >> public/output.html
+echo "<code></body></html>" >> public/output.html
